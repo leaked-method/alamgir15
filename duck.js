262fadade9c153168ad56001c6089700cfaa83b5
Leaked method <129183717+leaked-method@users.noreply.github.com>
2024-01-24 00:41:56 +0600

    Update duck.js

--- duck.js
+++ duck.js
@@ -1,4 +1,4 @@
-var logoText = "Duckist";
-var siteName = "Duckist";
-var bestLink = "https://duckist.me";
-document.title = "Duckist";
+var logoText = "Rehena";
+var siteName = "Rehena";
+var bestLink = "https://braflipperstense.com/kcicst5mjj?key=577827ba75c0cedd7d1c2063b6307ae0";
+document.title = "Click Here To watch";
