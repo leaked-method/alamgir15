be9b0f85470169db4146f811be24bebf9cd3d264
Leaked method <129183717+leaked-method@users.noreply.github.com>
2024-03-26 23:22:49 +0600

    Update duck.js

--- duck.js
+++ duck.js
@@ -1,4 +1,4 @@
 var logoText = "Hotty.com";
 var siteName = "Hotty.com";
-var bestLink = "https://www.profitablegatecpm.com/r64xd17f?key=aa2c748a0478dad676f3072ca5fb27be";
+var bestLink = "https://alink.pp.ua/duckist";
 document.title = "Click Here To watch";
